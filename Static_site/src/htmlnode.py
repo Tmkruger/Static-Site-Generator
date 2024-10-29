445f118fb3fd422d5f9f564f600974c23f50a5af
Tmkruger <tmkruger60@gmail.com>
2024-10-28 17:00:36 -0700

    Created LeafNode Class

--- Static_site/src/htmlnode.py
+++ Static_site/src/htmlnode.py
@@ -1,5 +1,5 @@
 class HTMLNode():
-	def __init__(self, tag=None, value=None, children=None, props=None):
+	def __init__(self, value=None,tag=None, children=None, props=None):
 		self.tag = tag
 		self.value = value
 		self.children = children
@@ -23,3 +23,22 @@ class HTMLNode():
 	def __repr__(self):
 		content = HTMLNode("h1", "This is a Header")
 		return f"HTMLNODE({content})"
+
+class LeafNode(HTMLNode):
+	def __init__(self, value, tag=None, props=None):
+		super().__init__()
+		self.value = value
+		self.tag = tag
+		self.props = props
+
+	def to_html:
+		if self.value == None:
+			raise ValueError("All leaf nodes must have a value")
+		if self.tag == None:
+			return self.value
+		else:
+			if self.tag == "a":
+				url_tag_value = list(self.props.items())
+				return f"<{self.tag} {url_tag_value[0]}={url_tag_value[1]}>{self.value}</{self.tag}>"
+			else:
+				return f"<{self.tag}>{self.value}</{self.tag}>"
--- Static_site/src/test_htmlnode.py
+++ Static_site/src/test_htmlnode.py
@@ -6,28 +6,30 @@ class TestHTMLNode(unittest.TestCase):
 
 	def test_eq(self):
 		print("IS EQUAL? HTML")
-		node = HTMLNode("p", "This is a Paragraph")
-		node2 = HTMLNode("p", "This is a Paragraph")
+		node = HTMLNode("This is a Paragraph", "p")
+		node2 = HTMLNode("This is a Paragraph", "p")
 		self.assertEqual(node, node2)
 
 	def test_eq_link(self):
 		print("IS EQUAL? HTML LINK")
-		link1 = HTMLNode("a", "Link to Boot.dev", props={"href": "https://www.boot.dev"})
-		link2 = HTMLNode("a", "Link to Boot.dev", props={"href": "https://www.boot.dev"})
+		link1 = HTMLNode("Link to Boot.dev", "a", props={"href": "https://www.boot.dev"})
+		link2 = HTMLNode("Link to Boot.dev", "a", props={"href": "https://www.boot.dev"})
 		self.assertEqual(link1, link2)
 
 	def test_eq_div_child(self):
 		print("IS EQUAL? HTML CHILD")
-		link1 = HTMLNode("a", "Link to Boot.dev", props={"href": "https://www.boot.dev"})
-		link2 = HTMLNode("a", "Link to Boot.dev", props={"href": "https://www.boot.dev"})
-		div_container = HTMLNode("div", children=link1)
-		div_container2 = HTMLNode("div", children=link2)
+		link1 = HTMLNode("Link to Boot.dev", "a", props={"href": "https://www.boot.dev"})
+		link2 = HTMLNode("Link to Boot.dev", "a", props={"href": "https://www.boot.dev"})
+		div_container = HTMLNode(tag="div", children=link1)
+		div_container2 = HTMLNode(tag="div", children=link2)
 		self.assertEqual(div_container, div_container2)
 
 	def test_not_eq_div_child(self):
 		print("IS NOT EQUAL? HTML CHILD")
-		link1 = HTMLNode("a", "Link to Boot.dev", props={"href": "https://www.boot.dev"})
-		link3 = HTMLNode("a", "Link to Youtube.com", props={"href": "https://www.youtube.com/"})
+		link1 = HTMLNode("Link to Boot.dev", "a", props={"href": "https://www.boot.dev"})
+		link3 = HTMLNode("Link to Youtube.com", "a", props={"href": "https://www.youtube.com/"})
 		div_container = HTMLNode("div", children=link1)
 		div_container3 = HTMLNode("div", children=link3)
-		self.assertNotEqual(div_container, div_container3)	
+		self.assertNotEqual(div_container, div_container3)
+
+	def test_leafnode_eq(self):
