91db1b0f471b0a44aa9d4d02cec988a9c6347784
Tristan Kruger <Tmkruger60@gmail.com>
2025-08-15 12:15:22 -0700

    static to public nonsense going on in main

--- src/main.py
+++ src/main.py
@@ -2,33 +2,39 @@ from textnode import TextNode , TextType
 from htmlnode import HtmlNode, LeafNode, ParentNode
 from nodemanager import split_nodes_delmiter
 from markdown_to_html import *
+import os
+import shutil
+
 
 
 def main():
-    markdown = """
-This is **bolded** paragraph
+    static_to_public("static", "public")
 
-This is another paragraph with *italic* text and code: `print("Hello World")` here
-This is _the_ same paragraph on a new line, ending **bold**
-
-```
-This is text that _should_ remain
-the **same** even with inline stuff
-```
-
-- This is a list
-- with items
-"""
-
-    block = """
-```
-This is text that _should_ remain
-the **same** even with inline stuff
-```
-"""
-    markdown_to_html_node(block)
-
-
-
+def static_to_public(source, destination):
+    print(f"SOURCE: {source} | DESTINATION: {destination}")
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+    if os.path.isfile(source) != True:
+        dirs = os.listdir(source)
+        print(f"DIRS: {dirs}")
+        for dir in dirs:
+            print(f"DIR: {dir}")
+            if dir == ".git":
+                continue
+            elif dir == ".DS_Store":
+                continue
+            elif os.path.isfile(f"{source}/{dir}") != True:
+                print(f"Going deeper into {dir}")
+                print(f"MAKING DIR {os.path.join(destination, dir)}")
+                os.mkdir(os.path.join(destination, dir))
+                new_destination = os.path.join(destination, dir)
+                static_to_public(os.path.join(source, dir), new_destination)
+            elif os.path.isfile(f"{source}/{dir}") == True:
+                print(f"Copying file {os.path.join(source, dir)} to {destination}")
+                shutil.copy(os.path.join(source, dir), destination)
+    else:
+        print(f"ADDING {source} TO {destination}")
+        shutil.copy(source, destination)
 
 main()
